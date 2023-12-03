0b449b777b23cd765f17a9b421cf1dcf41afeb33
shubham-sutar <shubhamsutar93@gmail.com>
2023-12-03 13:20:04 +0530

    change

--- 01_basic_html/Array.js
+++ 01_basic_html/Array.js
@@ -1,6 +1,30 @@
-console.log("-------------- video 60 ---------------");
+console.log("-------------- video 63 ---------------");
 
 let arr = [1,2,3,4,5,6];
 console.log(arr);
 console.log(arr.length);
 console.log(arr[3]);
+console.log(arr[0]=56);
+console.log(arr);
+
+//console.log(arr.toString());
+//console.log(typeof arr);
+
+let myArr = [0,2,3,6,7,8]
+console.log(myArr);
+
+//POP
+console.log(myArr.pop());
+console.log(myArr);
+
+//PUSH
+console.log(myArr.push(45));
+console.log(myArr);
+
+//Shift
+console.log(myArr.shift());
+console.log(myArr);
+
+//UNSSHIFT
+console.log(myArr.unshift(90))
+console.log(myArr)
